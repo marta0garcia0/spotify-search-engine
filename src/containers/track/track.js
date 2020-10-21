5638d9f3afe35dfaf1064e54344cbf325774bbc5
Marta García <marta.garcia07@hotmail.com>
2020-10-21 10:15:52 +0200

    fix

--- src/containers/track/track.js
+++ src/containers/track/track.js
@@ -32,27 +32,29 @@ class Track extends React.Component {
 
   render() {
     return this.props.token ? 
+      (this.props.player ?
       <div className='newsearch-container'>
-        <div className='newsearch-container_header'>
-          <FontAwesomeIcon icon={faTimes} onClick={() => {this.onClose();}}/>
-        </div>
-        <div className='track-container'>
-          <div className='track__image'>
-            <img src={this.props.player.album.images && this.props.player.album.images.length > 1 ?
-                this.props.player.album.images[1].url : ''} />
+          <div className='newsearch-container_header'>
+            <FontAwesomeIcon icon={faTimes} onClick={() => {this.onClose();}}/>
           </div>
-          <div className='track__info'>
-            <h3>Name: { this.props.player.name }</h3>
-            <h5>Album: { this.props.player.album.name }</h5>
-            <h5>Release date: { this.props.player.album.release_date }</h5>
+          <div className='track-container'>
+            <div className='track__image'>
+              <img src={this.props.player.album.images && this.props.player.album.images.length > 1 ?
+                  this.props.player.album.images[1].url : ''} />
+            </div>
+            <div className='track__info'>
+              <h3>Name: { this.props.player.name }</h3>
+              <h5>Album: { this.props.player.album.name }</h5>
+              <h5>Release date: { this.props.player.album.release_date }</h5>
+            </div>
           </div>
+          <div className='track-container__player'>
+            <div className='track__player' >
+              {this.player()}
+            </div>
         </div>
-        <div className='track-container__player'>
-          <div className='track__player' >
-            {this.player()}
-          </div>
         </div>
-      </div>
+        : '')
       :
       <Redirect to={{ pathname: '/login' }} />
   }
